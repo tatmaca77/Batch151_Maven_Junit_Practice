259d41508493ce771067851c84f967ef6c23b3ab
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-06-23 22:48:38 +0200

    23/06/23 JUnit Practice

---
+++ src/test/java/day07_practice/C01_Faker.java
@@ -0,0 +1,67 @@
+package day07_practice;
+
+import utilities.TestBase01;
+import com.github.javafaker.Faker;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+
+public class C01_Faker extends TestBase01 {
+
+    // 'https://www.facebook.com' sayfasina gidiniz
+    // yeni hesap olustur butonuna basin
+    // isim kutusunu locate ediniz
+    // geriye kalan alanlari TAB ile dolasarak faker bilgilerle formu doldurun
+
+    @Test
+    public void test01() {
+
+        // 'https://www.facebook.com' sayfasina gidiniz
+        driver.get("https://www.facebook.com");
+
+        // yeni hesap olustur butonuna basin
+        driver.findElement(By.xpath("//a[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']")).click();
+
+        // isim kutusunu locate ediniz
+        WebElement isimKutusu = driver.findElement(By.xpath("//input[@name='firstname']"));
+
+        // geriye kalan alanlari TAB ile dolasarak faker bilgilerle formu doldurun
+
+        Faker faker = new Faker();
+
+        String email = faker.internet().emailAddress();
+
+        Actions actions = new Actions(driver);
+
+        actions.click(isimKutusu).
+                sendKeys(faker.name().firstName()).
+                sendKeys(Keys.TAB).
+                sendKeys(faker.name().lastName()).
+                sendKeys(Keys.TAB).
+                sendKeys(email).
+                sendKeys(Keys.TAB).
+                sendKeys(email).
+                sendKeys(Keys.TAB).
+                sendKeys(faker.internet().password()).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys("15").
+                sendKeys(Keys.TAB).
+                sendKeys("Mart").
+                sendKeys(Keys.TAB).
+                sendKeys("2000").
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.ARROW_RIGHT).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.TAB).
+                sendKeys(Keys.ENTER).perform();
+
+    }
+}
