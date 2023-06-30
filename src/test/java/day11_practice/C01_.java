746dedb98e8ed3cd4c8bf6513c17c9a1f0fa4546
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-06-30 16:08:47 +0200

    30/06/23 JUnit Practice

---
+++ src/test/java/day11_practice/C01_.java
@@ -0,0 +1,6 @@
+package day11_practice;
+
+import utilities.TestBase01;
+
+public class C01_ extends TestBase01 {
+}
---
+++ src/test/java/day12_practice/C01_.java
@@ -0,0 +1,6 @@
+package day12_practice;
+
+import utilities.TestBase01;
+
+public class C01_ extends TestBase01 {
+}
