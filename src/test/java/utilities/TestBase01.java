d6e607fb85c6e4b271ac58fad59394ff31bd392a
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-06-30 16:05:30 +0200

    30/06/23 JUnit Practice

--- src/test/java/utilities/TestBase01.java
+++ src/test/java/utilities/TestBase01.java
@@ -1,17 +1,39 @@
 package utilities;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public abstract class TestBase01 {   // abstract yaptik ve bunun icinde obje olusturulmasini önleriz.
                                    // abstract zorunda degiliz; ancak buirasi bir depodur.
+
+    // protected yaptik cünkü bunlari farkli Package'larda kullanabilmek icin. extends yaptiktan sonra.
+    protected static ExtentReports extentReports; //-->Raporlamayı başlatmak için kullanılan class
+    protected static ExtentHtmlReporter extentHtmlReporter;//-->Raporu HTML formatında düzenler
+    //protected static ExtentTest extentTest;//--> Test adımlarına eklemek istediğimiz bilgileri bu class ile oluştururuz
 
     /**
        Bu Class'a "extends" ettigimiz Test Classlarindan ulasabiliriz.
@@ -40,6 +62,19 @@ public abstract class TestBase01 {   // abstract yaptik ve bunun icinde obje olu
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /** AlertWait methodu */
+    public void alertWait(int saniye) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(saniye));
+        wait.until(ExpectedConditions.alertIsPresent());
+    }
+
+    /** FluentWait visible Methodu */
+    public void visibleFluentWait(WebElement element, int saniye, int milisaniye) {
+        new FluentWait<>(driver).withTimeout(Duration.ofSeconds(saniye)).
+                pollingEvery(Duration.ofMillis(milisaniye)).
+                until(ExpectedConditions.visibilityOf(element));
     }
 
     /** AcceptAlert */
@@ -78,6 +113,103 @@ public abstract class TestBase01 {   // abstract yaptik ve bunun icinde obje olu
     public void selectValue(WebElement ddm,String value){
         Select select = new Select(ddm);
         select.selectByValue(value);
+    }
+
+    /** Explicit Wait */
+    public void visibleWait1(WebElement locate, int seconds ){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(seconds));
+        wait.until(ExpectedConditions.visibilityOf(locate));
+
+    }
+
+    /** SwitchTo Window-1 */
+    public void switchToWindow(int index){
+        List<String> pencereler = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(pencereler.get(index));
+    }
+
+    /** SwitchTo Window-2 */
+    public void switchWindow(int index){
+        driver.switchTo().window(driver.getWindowHandles().toArray()[index].toString());
+    }
+
+    /** Tüm Sayfa Resmi (ScreenShot) */
+    public void tumSayfaResmi() {
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "src/test/java/techproed/TumSayfaResmi/screenShot" + tarih + ".jpeg";
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        try {
+            FileUtils.copyFile(ts.getScreenshotAs(OutputType.FILE),new File(dosyaYolu));
+            //Files.write(Paths.get(dosyaYolu),ts.getScreenshotAs(OutputType.BYTES));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        /** Method olustururken mutlaka try-catch icinde Exception Handle etmelisin. !!!!!
+         Eger Method icinde Signature'a atarsak bu sefer her methodu cagirdigimizda bizden exception ister
+         Bu sebeple "try-catch" kullandik.
+         */
+    }
+
+    /** Web Element Resmi (WebElement ScreenShot) */
+    public void webElementResmi (WebElement element){
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date());
+        String dosyaYolu = "src/test/java/techproed/ElementResmi/WEscreenShot" + tarih + ".jpeg";
+        try {
+            FileUtils.copyFile(element.getScreenshotAs(OutputType.FILE),new File(dosyaYolu));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        /**
+         Her bu methodu cagirdigimda Exception ile ugrasmamak icin "try-catch" blogu kullaniriz. !!!!!
+         */
+    }
+
+    /** UploadFile Robot Class */
+
+    public void uploadFilePath(String filePath) {
+        try {
+            bekle(3);
+            StringSelection stringSelection = new StringSelection(filePath);
+            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+            Robot robot = new Robot();
+            robot.keyPress(KeyEvent.VK_CONTROL);
+            bekle(3);
+            robot.keyPress(KeyEvent.VK_V);
+            bekle(3);
+            robot.keyRelease(KeyEvent.VK_CONTROL);
+            bekle(3);
+            robot.keyRelease(KeyEvent.VK_V);
+            bekle(3);
+            robot.keyPress(KeyEvent.VK_ENTER);
+            bekle(3);
+            robot.keyRelease(KeyEvent.VK_ENTER);
+            bekle(3);
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /** Extent Report Methodu */
+
+    public void extentReport(String browser, String reportName, String testerName){
+        extentReports = new ExtentReports();
+        String tarih = new SimpleDateFormat("_hh_mm_ss_ddMMyyyy").format(new Date()); // Her raporu ayri ayri olusturur.
+        String dosyaYolu = "testOutput/extentReports/extentReport"+tarih+".html"; // DOSYA YOLU belirttik.
+        extentHtmlReporter = new ExtentHtmlReporter(dosyaYolu);
+        extentReports.attachReporter(extentHtmlReporter);//-->HTML formatında raporlamayı başlatacak
+
+        //Raporda gözükmesini isteğimiz bilgiler için
+        extentReports.setSystemInfo("Browser",browser);
+        extentReports.setSystemInfo("Tester",testerName);
+        extentHtmlReporter.config().setDocumentTitle("Extent Report");
+        extentHtmlReporter.config().setReportName(reportName);
+
+        /**
+         Extent Report alabilmek icin yaptigim ayarlamalarin methodu. Class isimlerini de Class seviyesinde en üstte
+         yazdik.
+         */
+
     }
 
 }
