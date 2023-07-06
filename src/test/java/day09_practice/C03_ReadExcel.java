81ddbdf55014acdb29b2bb36f361be07e15031ac
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-06 16:45:45 +0200

    05/07/23 JUnit Practice

---
+++ src/test/java/day09_practice/C03_ReadExcel.java
@@ -0,0 +1,42 @@
+package day09_practice;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class C03_ReadExcel {
+
+    /**
+     // Ulkeler dosyasındaki 64.satır indexin, 1.sutunundaki indexinin
+     // "Avusturya" oldugunu test eden "method" olusturunuz.
+     */
+
+    @Test
+    public void excelReadTest01() throws IOException {
+
+        int rowNumIdx = 64;
+        int cellNumIdx = 1;
+
+        String actualData = banaDataGetir(rowNumIdx, cellNumIdx);
+        String expectedData = "Berlin";
+
+        Assert.assertEquals(expectedData, actualData);
+
+    }
+
+    public String banaDataGetir(int rowNumIdx, int cellNumIdx) throws IOException {
+
+        String filePath = "src/resources/ulkeler.xlsx";
+        FileInputStream fis = new FileInputStream(filePath);
+        Workbook workbook = WorkbookFactory.create(fis);
+
+        String istenenData = workbook.getSheet("Sayfa1").getRow(rowNumIdx).getCell(cellNumIdx).toString();
+
+        return istenenData;
+
+    }
+}
