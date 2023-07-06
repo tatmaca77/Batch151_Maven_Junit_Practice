f87bff46420430f48f2717af5b19ffc3cef6fc8b
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-06 17:15:36 +0200

    05/07/23 JUnit Practice

--- src/test/java/day09_practice/C05_ReadExcel.java
+++ src/test/java/day09_practice/C05_ReadExcel.java
@@ -16,6 +16,12 @@ public class C05_ReadExcel {
     @Test
     public void excelReadTest01() throws IOException {
 
+        /**
+        Bu soruda banaDataGetir methodu kullandik !!!
+         */
+
+        /** 1. WAY */
+
         String filePath = "src/resources/ulkeler.xlsx";
         FileInputStream fis = new FileInputStream(filePath);
         Workbook workbook = WorkbookFactory.create(fis);
@@ -28,6 +34,29 @@ public class C05_ReadExcel {
             String satirdakiData = C01_ReadExcel.banaDataGetir(i,3);
             System.out.println("satirdakiData = " + satirdakiData);
 
+        }
+
+    }
+
+    @Test
+    public void excelReadTest02() throws IOException {
+
+        /**
+         Bu soruda banaDataGetir methodu kullanmak zorunda degiliz. !!!
+         */
+
+        /** 2. WAY */
+
+        String filePath = "src/resources/ulkeler.xlsx";
+        FileInputStream fis = new FileInputStream(filePath);
+        Workbook workbook = WorkbookFactory.create(fis);
+
+        int lastRowNumIdx = workbook.getSheet("Sayfa1").getLastRowNum();
+        System.out.println("lastRowNumIdx = " + (lastRowNumIdx+1));
+
+        for (int i = 0; i < lastRowNumIdx; i++) {
+            String satirdakiData = workbook.getSheet("Sayfa1").getRow(i).getCell(3).toString();
+            System.out.println("satirdakiData = " + satirdakiData);
         }
 
     }
