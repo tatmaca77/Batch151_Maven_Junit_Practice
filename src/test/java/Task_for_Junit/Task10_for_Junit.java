0c62dc3e4f618d4ce4aabcfa5509269e5ef3fa3e
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-09 02:42:26 +0200

    08/07/23 JUnit Practice

---
+++ src/test/java/Task_for_Junit/Task10_for_Junit.java
@@ -0,0 +1,15 @@
+package Task_for_Junit;
+
+import utilities.TestBase01;
+
+public class Task10_for_Junit extends TestBase01 {
+
+    /**
+    //https://www.techlistic.com/p/demo-selenium-practice.html adresine gidelim
+//Demo Table altındaki tablodaki otel bilgilerini yazdırınız
+//Demo Table altındaki otel bilgileri olan tablodan Height değeri 601m olan otelin bilgilerini yazdırınız
+//Bütün Height bilgilerini yazdırınız
+//Otel uzunluklarının hepsini toplayınız
+//Bir method ile satır ve sutun bilgilerini çağıralım
+     */
+}
