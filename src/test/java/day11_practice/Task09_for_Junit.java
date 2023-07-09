3a2b7040993912775c23c52965f531e6d2585629
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-09 02:39:33 +0200

    08/07/23 JUnit Practice

---
+++ src/test/java/day11_practice/Task09_for_Junit.java
@@ -0,0 +1,24 @@
+package day11_practice;
+
+import utilities.TestBase01;
+
+public class Task09_for_Junit extends TestBase01 {
+    /**
+    //https://www.selenium.dev/downloads/ adresine gidelim
+//Sayfanın resmini alalım
+//Platforms Supported by Selenium altındaki Browsers bölümü görünene kadar sayfayı indirelim
+//Browser bölümünden Chrome driver bölümündeki documentation linkine tıklıyalım
+//Documentation webelementinin resmini alalım
+//All versions available in Downloads altında Latest stable release olan linki tıklayalım
+//Açılan pencerede chromedriver'i indirelim
+//Driver'in indiğini doğrulayalım
+//İndirmiş olduğumuz dosyayı silelim
+//Silindiğini doğrulayalım
+//Not: Bu task'in her adımı için Html rapor oluşturalım(Extent Report)
+
+     */
+
+
+
+
+}
