d512a0ab4ce93b678436c4a608be82e70507f37e
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-09 03:23:14 +0200

    08/07/23 JUnit Practice

--- src/test/java/Task_for_Junit/Task10_for_Junit.java
+++ src/test/java/Task_for_Junit/Task10_for_Junit.java
@@ -1,6 +1,11 @@
 package Task_for_Junit;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utilities.TestBase01;
+
+import java.util.List;
 
 public class Task10_for_Junit extends TestBase01 {
 
@@ -12,4 +17,35 @@ public class Task10_for_Junit extends TestBase01 {
 //Otel uzunluklarının hepsini toplayınız
 //Bir method ile satır ve sutun bilgilerini çağıralım
      */
+
+    @Test
+    public void test01() {
+        //https://www.techlistic.com/p/demo-selenium-practice.html adresine gidelim
+        driver.navigate().to("https://www.techlistic.com/p/demo-selenium-practice.html");
+
+
+        //Demo Table2 altındaki tablodaki otel bilgilerini yazdırınız
+        List<WebElement> demoTableHotelList = driver.findElements(By.xpath("(//tbody)[2]//th"));
+
+        demoTableHotelList.forEach(t-> System.out.println(t.getText()));
+
+
+        //Demo Table2 altındaki otel bilgileri olan tablodan Height değeri 601m olan otelin bilgilerini yazdırınız
+
+
+
+
+
+        //Bütün Height bilgilerini yazdırınız
+
+
+
+
+
+
+
+
+
+
+    }
 }
