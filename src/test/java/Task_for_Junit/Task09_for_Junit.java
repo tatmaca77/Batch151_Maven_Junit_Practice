d613aa53e3347303e72598eb44f3d70449a76cd8
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-17 16:09:39 +0200

    17/07/23 JUnit Practice

--- src/test/java/Task_for_Junit/Task09_for_Junit.java
+++ src/test/java/Task_for_Junit/Task09_for_Junit.java
@@ -27,37 +27,46 @@ public class Task09_for_Junit extends TestBase01 {
 
     ExtentReports extentReports; //-->Raporlamayı başlatmak için kullanılan class
     ExtentHtmlReporter extentHtmlReporter; //-->Raporu HTML formatında düzenler
-    ExtentTest extentTest;
+    ExtentTest extentTest; //--> Test adımlarına eklemek istediğimiz bilgileri bu class ile oluştururuz
 
     @Test
     public void test01() {
+
+        extentReport("Chrome", "Selenium Dev Testi", "Tolgahan");
+        extentTest = extentReports.createTest("ExtentReport","Test Raporu"); // Test islemlerini baslatir.
+
+
         //https://www.selenium.dev/downloads/ adresine gidelim
         driver.navigate().to("https://www.selenium.dev/downloads/");
+        extentTest.info("Selenium Dev sayfasina gidildi.");
 
 
         //Sayfanın resmini alalım
         tumSayfaResmi();
+        extentTest.info("Tum Sayfanin ekran görüntüsü alindi.");
 
 
         //Platforms Supported by Selenium altındaki Browsers bölümü görünene kadar sayfayı indirelim
-
         Actions actions = new Actions(driver);
         WebElement browser = driver.findElement(By.xpath("(//button[contains(@class,'btn btn-dark')])[1]"));
         actions.scrollToElement(browser);
+        extentTest.info("Browser WebElemntine kadar safya asagiya kaydirildi.");
+
 
 
         //Browser bölümünden Chrome driver bölümündeki documentation linkine tıklıyalım
-
         browser.click();
         WebElement documentationChrome = driver.findElement(By.xpath("//a[@href='https://chromedriver.chromium.org/']"));
         actions.scrollToElement(documentationChrome);
         documentationChrome.click();
+        extentTest.info("Browser WE tiklandi, DocumentationChrome'a kadar Scroll yapildi ve tiklandi.");
 
 
 
         //Documentation webelementinin resmini alalım
         WebElement documentation = driver.findElement(By.xpath("(//a[text()='Documentation'])[2]"));
         webElementResmi(documentation);
+        extentTest.info("Documentation WebElemnti'nin ekran resmi alindi.");
 
 
 
@@ -65,6 +74,10 @@ public class Task09_for_Junit extends TestBase01 {
         driver.findElement(By.xpath("(//a[@data-url='/downloads'])[2]")).click();  //
         driver.findElement(By.xpath("//span[text()='Version Selection']")).click();
         driver.findElement(By.xpath("//a[text()='https://chromedriver.storage.googleapis.com/LATEST_RELEASE']")).click();
+
+
+
+
 
 
 
