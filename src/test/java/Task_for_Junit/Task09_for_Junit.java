84d721818a0c79c35ee7782d42306dde622bcfb1
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-07-17 15:54:13 +0200

    17/07/23 JUnit Practice

--- src/test/java/Task_for_Junit/Task09_for_Junit.java
+++ src/test/java/Task_for_Junit/Task09_for_Junit.java
@@ -1,5 +1,9 @@
 package Task_for_Junit;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase01;
 
 public class Task09_for_Junit extends TestBase01 {
@@ -18,7 +22,68 @@ public class Task09_for_Junit extends TestBase01 {
 
      */
 
+    @Test
+    public void test01() {
+        //https://www.selenium.dev/downloads/ adresine gidelim
+        driver.navigate().to("https://www.selenium.dev/downloads/");
+
+
+        //Sayfanın resmini alalım
+        tumSayfaResmi();
+
+
+        //Platforms Supported by Selenium altındaki Browsers bölümü görünene kadar sayfayı indirelim
+
+        Actions actions = new Actions(driver);
+        WebElement browser = driver.findElement(By.xpath("(//button[contains(@class,'btn btn-dark')])[1]"));
+        actions.scrollToElement(browser);
+
+
+        //Browser bölümünden Chrome driver bölümündeki documentation linkine tıklıyalım
+
+        browser.click();
+        WebElement documentationChrome = driver.findElement(By.xpath("//a[@href='https://chromedriver.chromium.org/']"));
+        actions.scrollToElement(documentationChrome);
+        documentationChrome.click();
 
 
 
+        //Documentation webelementinin resmini alalım
+        WebElement documentation = driver.findElement(By.xpath("(//a[text()='Documentation'])[2]"));
+        webElementResmi(documentation);
+
+
+
+        //All versions available in Downloads altında Latest stable release olan linki tıklayalım
+        driver.findElement(By.xpath("(//a[@data-url='/downloads'])[2]")).click();  //
+        driver.findElement(By.xpath("//span[text()='Version Selection']")).click();
+        driver.findElement(By.xpath("//a[text()='https://chromedriver.storage.googleapis.com/LATEST_RELEASE']")).click();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
